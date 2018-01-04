7ee407fbbbc0a002e9197fe47f17a5af96023574
TopItHero <andrew.vmail7@gmail.com>
2018-01-05 00:23:04 +0200

    category working now

--- shoponline/urls.py
+++ shoponline/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     url(r'^homepage', include('homepage.urls')),
     url(r'^', include('orders.urls')),
     url(r'^products/', include('products.urls')),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
